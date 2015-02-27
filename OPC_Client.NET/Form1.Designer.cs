648f16639b69418205f520d142e5318f0fd0c07f
ShanksVision <shankar86ssn@gmail.com>
2015-02-26 16:19:54 -0800

    First upload

---
+++ OPC_Client.NET/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace OPC_Client.NET
+{
+    partial class frmOPCClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lBoxOPCServers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lBoxTags = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddTag = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTagName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTagValue = new System.Windows.Forms.Label();
+            this.btnRefreshServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lBoxOPCServers
+            // 
+            this.lBoxOPCServers.FormattingEnabled = true;
+            this.lBoxOPCServers.Location = new System.Drawing.Point(12, 54);
+            this.lBoxOPCServers.Name = "lBoxOPCServers";
+            this.lBoxOPCServers.Size = new System.Drawing.Size(201, 147);
+            this.lBoxOPCServers.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Available OPC Servers ";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 225);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(111, 37);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lBoxTags
+            // 
+            this.lBoxTags.FormattingEnabled = true;
+            this.lBoxTags.Location = new System.Drawing.Point(307, 54);
+            this.lBoxTags.Name = "lBoxTags";
+            this.lBoxTags.Size = new System.Drawing.Size(226, 147);
+            this.lBoxTags.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(304, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Available Tags";
+            // 
+            // btnAddTag
+            // 
+            this.btnAddTag.Location = new System.Drawing.Point(307, 225);
+            this.btnAddTag.Name = "btnAddTag";
+            this.btnAddTag.Size = new System.Drawing.Size(111, 37);
+            this.btnAddTag.TabIndex = 5;
+            this.btnAddTag.Text = "Add Tag";
+            this.btnAddTag.UseVisualStyleBackColor = true;
+            this.btnAddTag.Click += new System.EventHandler(this.btnAddTag_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tag Name : ";
+            // 
+            // lblTagName
+            // 
+            this.lblTagName.AutoSize = true;
+            this.lblTagName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTagName.Location = new System.Drawing.Point(115, 306);
+            this.lblTagName.Name = "lblTagName";
+            this.lblTagName.Size = new System.Drawing.Size(0, 16);
+            this.lblTagName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(304, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tag Value : ";
+            // 
+            // lblTagValue
+            // 
+            this.lblTagValue.AutoSize = true;
+            this.lblTagValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTagValue.Location = new System.Drawing.Point(401, 306);
+            this.lblTagValue.Name = "lblTagValue";
+            this.lblTagValue.Size = new System.Drawing.Size(0, 16);
+            this.lblTagValue.TabIndex = 9;
+            // 
+            // btnRefreshServer
+            // 
+            this.btnRefreshServer.Location = new System.Drawing.Point(219, 54);
+            this.btnRefreshServer.Name = "btnRefreshServer";
+            this.btnRefreshServer.Size = new System.Drawing.Size(82, 37);
+            this.btnRefreshServer.TabIndex = 10;
+            this.btnRefreshServer.Text = "Refresh Server List";
+            this.btnRefreshServer.UseVisualStyleBackColor = true;
+            this.btnRefreshServer.Click += new System.EventHandler(this.btnRefreshServer_Click);
+            // 
+            // frmOPCClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 342);
+            this.Controls.Add(this.btnRefreshServer);
+            this.Controls.Add(this.lblTagValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTagName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAddTag);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lBoxTags);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lBoxOPCServers);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmOPCClient";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OPC .NET Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmOPCClient_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lBoxOPCServers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ListBox lBoxTags;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddTag;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTagName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTagValue;
+        private System.Windows.Forms.Button btnRefreshServer;
+    }
+}
+
---
+++ OPC_Client.NET/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OPCAutomation;
+
+namespace OPC_Client.NET
+{
+    public partial class frmOPCClient : Form
+    {
+        private OPCTag[] tags;
+        private bool updateOPC = false;
+        private int tagCount = Properties.Settings.Default.TagCount;
+        private OPCServer mServer;
+        private OPCGroups mGroups;
+        private OPCGroup mGroup;
+        private OPCBrowser mBrowser;
+        
+        private bool mServerConnected;
+
+        private const int DEFAULT_GROUP_UPDATE_RATE = 50;
+        private const int DEFAULT_GROUP_DEAD_BAND = 0;
+        private const bool DEFAULT_GROUP_IS_ACTIVE = true;       
+
+        public frmOPCClient()
+        {
+            InitializeComponent();
+            tags = new OPCTag[tagCount];
+            for (int i = 0; i < tagCount; i++)
+            {
+                tags[i] = new OPCTag();
+                tags[i].sensorName = Properties.Settings.Default.SensorName;
+                tags[i].tagName = Properties.Settings.Default.TagName;
+            }
+
+            GetOPCServerList();
+            
+        }
+
+        private void GetOPCServerList()
+        {            
+            try
+            {
+                this.mServer = new OPCServer();
+                object opcServerList = mServer.GetOPCServers(string.Empty);                
+                if(opcServerList != null && opcServerList is Array)
+                {
+                    Array opcServers = (Array)opcServerList;
+                    for(int i = opcServers.GetLowerBound(0); i <= opcServers.GetUpperBound(0); i++)
+                    {
+                        this.lBoxOPCServers.Items.Add(opcServers.GetValue(i).ToString());
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnRefreshServer_Click(object sender, EventArgs e)
+        {
+            this.lBoxOPCServers.Items.Clear();
+            this.GetOPCServerList();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lBoxOPCServers.Items == null || lBoxOPCServers.Items.Count == 0 || lBoxOPCServers.SelectedItem == null)
+                {
+                    MessageBox.Show("No OPC Servers Found or selected!!");
+                    return;
+                }
+
+                if (!mServerConnected)
+                {
+                    this.mServer = new OPCServer();
+                    this.mServer.Connect(lBoxOPCServers.SelectedItem.ToString(), string.Empty);
+                    this.mServerConnected = true;
+                    this.mGroups = mServer.OPCGroups;
+                    this.btnConnect.Text = "Disconnect";
+                    this.mServer.ClientName = "OPC_Client.NET";
+                    this.mServer.ServerShutDown += new DIOPCServerEvent_ServerShutDownEventHandler(mServerShutdown);
+                    MessageBox.Show("Connection Succesfull!!");
+                    this.PopulateTags();
+                }
+                else
+                {
+                    this.mServer.Disconnect();
+                    this.btnConnect.Text = "Connect";
+                    this.mServer.ServerShutDown -= new DIOPCServerEvent_ServerShutDownEventHandler(mServerShutdown);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection failed!!");
+            }
+           
+        }
+
+        private void PopulateTags()
+        {
+            try
+            {
+                if (this.tags == null)
+                    return;
+
+            string sSensor;
+
+            //Clear any current groups
+            if (this.mGroups != null)
+            {
+                this.mGroups.RemoveAll();
+            }
+
+            if (this.mGroup != null)
+                this.mGroup.DataChange -= new OPCAutomation.DIOPCGroupEvent_DataChangeEventHandler(this.mGroupDataChange);
+
+            this.mGroups.DefaultGroupUpdateRate = DEFAULT_GROUP_UPDATE_RATE;
+            this.mGroups.DefaultGroupDeadband = DEFAULT_GROUP_DEAD_BAND;
+            this.mGroups.DefaultGroupIsActive = DEFAULT_GROUP_IS_ACTIVE;
+
+            //Create a new group for the tags to be selected
+            this.mGroup = this.mGroups.Add("default");
+            this.mGroup.UpdateRate = 50;
+            this.mGroup.IsSubscribed = true;
+            this.mGroup.DataChange += new OPCAutomation.DIOPCGroupEvent_DataChangeEventHandler(this.mGroupDataChange);
+            
+            if (this.mServerConnected)
+            {
+                //create a new instance of a browser (view)
+                this.mBrowser = mServer.CreateBrowser();
+                //Set up the browser with basic/default values.
+                this.mBrowser.AccessRights = System.Convert.ToInt32(OPCAutomation.OPCAccessRights.OPCReadable); //view all tags
+                //Data type filter (0 for none)
+                this.mBrowser.DataType = 0;
+                //only sensors in the browser view
+                this.mBrowser.ShowBranches();
+
+                try
+                {
+                    int clienthandle = 1;
+                    foreach (OPCTag tempTag in this.tags)
+                    {
+
+                        sSensor = tempTag.sensorName;
+                        this.mBrowser.MoveToRoot();
+                        if(!string.IsNullOrWhiteSpace(sSensor))
+                        {
+                            try
+                            {
+                                this.mBrowser.MoveDown(sSensor);
+                            }
+                            catch
+                            {
+                                continue;
+                            }
+                        }
+                       
+                        // get the flat list of tags
+                        this.mBrowser.ShowLeafs(true);
+
+                        //Traverse through each tag of the selected sensor, storing it to the OPC Group
+                        for (int i = 1; i <= this.mBrowser.Count; i++)
+                        {
+                            string sValue = string.Empty;
+                            object vItemID = this.mBrowser.Item(i).ToString();
+                            string sItemID = vItemID.ToString();
+                            this.lBoxTags.Items.Add(sItemID);
+                            //ListViewItem oListViewItem = lstTags.Items.Add(sItemID);
+
+                            //Adding the new tag to the OPC Group.
+                            //if (sItemID.Contains(tempTag.tagName))
+                            //{
+                            //    this.mGroup.OPCItems.AddItem(sItemID, clienthandle);                                
+                            //    clienthandle++;
+                            //    break;
+                            //}
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Exception in Refresh the data on OPC server. Please make sure the OPC Server service is started.");
+                    updateOPC = true;
+                }
+            }
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Exception when populating tags from the selected OPC Server.");
+            }
+        }
+
+        private void frmOPCClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mServer.Disconnect();
+            mServer.ServerShutDown -= new DIOPCServerEvent_ServerShutDownEventHandler(mServerShutdown);
+        }
+
+        private void mServerShutdown(string Reason)
+        {
+            this.Cursor = Cursors.WaitCursor;           
+
+            //Removes all current OPCGroup and OPCItem objects to prepare for server shutdown.
+            if (mGroups != null)
+            {
+                mGroups.RemoveAll();
+                mServerConnected = false;
+                lBoxTags.Items.Clear();
+            }
+
+            this.Cursor = Cursors.Default;
+        }
+
+        private void mGroupDataChange(int TransactionID, int NumItems, ref System.Array ClientHandles, ref System.Array ItemValues, ref System.Array Qualities, ref System.Array TimeStamps)
+        {
+            //MessageBox.Show(ItemValues.GetValue(1).ToString() + " " + Convert.ToDateTime(TimeStamps.GetValue(1).ToString()).ToLocalTime().ToString());
+            for (int i = 1; i <= NumItems; i++)
+            {
+                string sensor = "";
+                string tag = "";
+                DateTime auditdate = Convert.ToDateTime(TimeStamps.GetValue(i).ToString()).ToLocalTime();
+                string data = ItemValues.GetValue(i).ToString();
+
+                string item = mGroup.OPCItems.Item(ClientHandles.GetValue(i)).ItemID;
+                string[] items = item.Split('.');
+                if (items.Length > 1)
+                {
+                    sensor = items[0];
+                    tag = items[1];
+                }
+
+                if (data != "")
+                {
+                    this.lblTagName.Text = tag;
+                    this.lblTagValue.Text = data;                    
+                }
+            }
+        }
+
+        private void btnAddTag_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(lBoxTags.Items == null || lBoxTags.Items.Count == 0 || lBoxTags.SelectedItem == null)
+                {
+                    MessageBox.Show("No tags found or selected!!");
+                    return;
+                }
+
+                
+                this.mGroup.OPCItems.AddItem(lBoxTags.SelectedItem.ToString(), 1);
+                string[] items = lBoxTags.SelectedItem.ToString().Split('.');
+                Properties.Settings.Default.SensorName = items[0];
+                Properties.Settings.Default.TagName = items[1];
+                Properties.Settings.Default.Save();
+                MessageBox.Show("Tag Added succesfully");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Exception while adding tags");
+            }
+        }
+
+
+
+
+    }
+
+    public class OPCTag
+    {
+        public string sensorName;
+        public string tagName;
+    }
+}
